21f0bcc8d0843f94e673ef9a881888fb9c6d42bd
Rajeev-Gupta555 <106828489+Rajeev-Gupta555@users.noreply.github.com>
2022-07-23 18:54:10 +0530

    Update imageToBin.py

--- src/imageToBin.py
+++ src/imageToBin.py
@@ -1,17 +1,24 @@
 from ast import Pass
 import cv2
+import numpy as np
+# reading image to numpy array
+image = cv2.imread("lion.jpg")
+print(image)
+# creating output file 
+coe = open("/home/rajeevuser/Desktop/verilog/data_in.txt", "w")
 
-image = cv2.imread("original_blur1.png")
-
-coe = open("input_blur_bin_FPGA.coe", "w")
-
-siz = image.shape
-print(siz)
+# print(siz)
 height=len(image)
+
+# *************write to file************
+
+# move in row
 for i in range(height):
     row=image[i]
+    # move in column
     for j in row:
         x = ""
+        # pixel to binary
         for k in j:
             bi = bin(k)[2:]
             for d in range(8-len(bi)):
@@ -20,5 +27,5 @@ for i in range(height):
         x = int(int(x[0:8],2)/3+int(x[8:16],2)/3+int(x[16:],2)/3)
         x = bin(x)[2:]
         n = len(x)
-        coe.write('0'*(8-n)+ x +  ' ')
+        coe.write('0'*(8-n)+ x +  '\n')
 coe.close() 
